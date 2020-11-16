52e7a1ea47ad331da59d93410a1c5422c43468fb
ilove2git <454677085@qq.com>
2020-11-16 11:30:33 +0800

    Update shell_unix.go

--- agent/session/shell/shell_unix.go
+++ agent/session/shell/shell_unix.go
@@ -68,12 +68,12 @@ func StartPty(
 	//Start the command with a pty
 	var cmd *exec.Cmd
 	if strings.TrimSpace(shellProps.Linux.Commands) == "" || isSessionLogger {
-	//	cmd = exec.Command("sh")
-		cmd = exec.Command("date")
+		log.Info("aaaa")
+		cmd = exec.Command("sh")
 	} else {
 		commandArgs := append(utility.ShellPluginCommandArgs, shellProps.Linux.Commands)
-	//	cmd = exec.Command("sh", commandArgs...)
-		cmd = exec.Command("date", commandArgs...)
+		log.Info("bbbb")
+		cmd = exec.Command("sh", commandArgs...)
 	}
 
 	//TERM is set as linux by pty which has an issue where vi editor screen does not get cleared.
