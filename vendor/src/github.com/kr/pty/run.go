f107c4e4e7839e760f3c0616523059252677ddee
ilove2git <454677085@qq.com>
2020-11-21 10:38:52 +0800

    Update run.go

--- vendor/src/github.com/kr/pty/run.go
+++ vendor/src/github.com/kr/pty/run.go
@@ -6,7 +6,8 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
-	log "github.com/aws/amazon-ssm-agent/agent/log"
+	"log"
+	"fmt"
 )
 
 // Start assigns a pseudo-terminal tty os.File to c.Stdin, c.Stdout,
@@ -18,27 +19,27 @@ func Start(c *exec.Cmd) (pty *os.File, err error) {
 		return nil, err
 	}
 	
-	log.Info("pty 1111111")
+	log.Println("pyt 11111111")
 	
 	defer tty.Close()
 	c.Stdout = tty
 	c.Stdin = tty
 	c.Stderr = tty
 	
-	log.Info("pty 22222222")
+	log.Println("pyt 22222222")
 	
 	if c.SysProcAttr == nil {
 		c.SysProcAttr = &syscall.SysProcAttr{}
-		log.Info("pty 3333333")
+		log.Println("pty 3333333")
 	}
 	c.SysProcAttr.Setctty = true
 	c.SysProcAttr.Setsid = true
 	
-	log.Info("pty 44444444")
+	log.Println("pty 44444444")
 	
 	err = c.Start()
 	if err != nil {
-		log.Info("pty 55555555")
+		log.Println("pty 55555555")
 		pty.Close()
 		return nil, err
 	}
